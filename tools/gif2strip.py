270e635489ee546e1381574ba59db84394405132
saluk <saluk64007@gmail.com>
2010-03-18 13:52:56 -0700

    Update gif2strip in tools with url fetch, and framedelay

--- tools/gif2strip.py
+++ tools/gif2strip.py
@@ -1,16 +1,46 @@
-import os,sys,subprocess,pygame,math
+import os,sys,subprocess,pygame,math,urllib2
+import re
 pygame.display.set_mode([1,1])
 
 MAX_SURFACE_WIDTH=1024
 
-def go(path_to_gif):
-    path_to_gif = sys.argv[1]
-    root = path_to_gif.rsplit(".",1)[0]
+def go(path_to_gif,saveto=None,delete=False):
+    if path_to_gif.startswith("http://"):
+        f = urllib2.urlopen(path_to_gif)
+        path_to_gif = path_to_gif.rsplit("/",1)[1]
+        out = open(path_to_gif,"wb")
+        out.write(f.read())
+        f.close()
+        out.close()
+    if not saveto:
+        root = path_to_gif.rsplit(".",1)[0]
+    else:
+        root = saveto.rsplit(".",1)[0]
     strip_name = root+".png"
     txt_name = root+".txt"
 
+    proc = subprocess.Popen('gifsicle -I "%s"'%(path_to_gif),stdout=subprocess.PIPE)
+    stdout,stderr = proc.communicate()
+    print stdout,stderr
+    loop = "loop forever" in stdout
+    delays = []
+    for x in stdout.split("  + ")[1:]:
+        delay = x.find(" delay ")
+        if delay==-1:
+            delays.append(None)
+            continue
+        s = re.findall("(\d+(\.\d+))s",x)
+        if s:
+            print s
+            delays.append(float(s[0][0]))
+    #delays are seconds: need frames, 60frames = 1 second
+    delays = [x*60.0 for x in delays if x]
+    print loop,delays
+    
     proc = subprocess.Popen('gifsicle --no-background -U -e "%s" -o "%s"'%(path_to_gif,path_to_gif))
     proc.wait()
+    if delete:
+        os.remove(path_to_gif)
     i = 0
     pygame_frames = []
     while True:
@@ -39,9 +69,23 @@ def go(path_to_gif):
         x+=f.get_width()
     pygame.image.save(out,strip_name)
     f = open(txt_name,"w")
-    f.write("horizontal %s\nvertical %s\nlength %s\nloops 1\n"%(in_row,num_rows,len(pygame_frames)))
+    f.write("horizontal %s\nvertical %s\nlength %s\n"%(in_row,num_rows,len(pygame_frames)))
+    if loop:
+        f.write("loops 1\n")
+    else:
+        f.write("loops 0\n")
+    for i,d in enumerate(delays):
+        if d:
+            d = int(d)
+            if d==0:
+                d=1
+            f.write("framedelay %s %s\n"%(i,d))
 
 if __name__ == "__main__":
-    if len(sys.argv)<2:
-        sys.argv.append(r"games\aaotest\art\port\aao_1\Ressources_Images_persos_PhoenixVieux_1.gif(talk).gif")
-    go(sys.argv[1])
+    go("hikaru-breakdown(a).gif")
+    go("miles-damage(d).gif")
+
+#~ if __name__ == "__main__":
+    #~ if len(sys.argv)<2:
+        #~ sys.argv.append(r"games\aaotest\art\port\aao_1\Ressources_Images_persos_PhoenixVieux_1.gif(talk).gif")
+    #~ go(sys.argv[1])
